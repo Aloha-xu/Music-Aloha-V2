4f0138366044732bc5799a270644cccf0ae62000
Alohaxuuuu <598392491@qq.com>
2022-05-19 11:37:16 +0800

    add 节流函数

--- src/utils/uctil.js
+++ src/utils/uctil.js
@@ -1,51 +1,100 @@
-// //防抖 
+// //防抖
 export function _debounce(fn, delay) {
   let timer = null;
   return (...args) => {
     if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
-      fn.apply(this, args)
-    }, delay)
-  }
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+//节流
+export function _throttle(fn, delay) {
+  var prev = Date.now();
+  return function() {
+    var now = Date.now();
+    if (now - prev > delay) {
+      fn();
+      prev = Date.now();
+    }
+  };
 }
 
 export function getYMDHMS(timestamp) {
-  let time = new Date(timestamp)
-  let year = time.getFullYear()
-  const month = (time.getMonth() + 1).toString().padStart(2, '0')
-  const date = (time.getDate()).toString().padStart(2, '0')
-  const hours = (time.getHours()).toString().padStart(2, '0')
-  const minute = (time.getMinutes()).toString().padStart(2, '0')
-  const second = (time.getSeconds()).toString().padStart(2, '0')
+  let time = new Date(timestamp);
+  let year = time.getFullYear();
+  const month = (time.getMonth() + 1).toString().padStart(2, "0");
+  const date = time
+    .getDate()
+    .toString()
+    .padStart(2, "0");
+  const hours = time
+    .getHours()
+    .toString()
+    .padStart(2, "0");
+  const minute = time
+    .getMinutes()
+    .toString()
+    .padStart(2, "0");
+  const second = time
+    .getSeconds()
+    .toString()
+    .padStart(2, "0");
 
-  return year + '年' + month + '月' + date + '日 ' + hours + ':' + minute + ':' + second
+  return (
+    year +
+    "年" +
+    month +
+    "月" +
+    date +
+    "日 " +
+    hours +
+    ":" +
+    minute +
+    ":" +
+    second
+  );
 }
 
 export function getYMD(timestamp) {
-  let time = new Date(timestamp)
-  let year = time.getFullYear()
-  const month = (time.getMonth() + 1).toString().padStart(2, '0')
-  const date = (time.getDate()).toString().padStart(2, '0')
-  return year + '年' + month + '月' + date + '日 '
+  let time = new Date(timestamp);
+  let year = time.getFullYear();
+  const month = (time.getMonth() + 1).toString().padStart(2, "0");
+  const date = time
+    .getDate()
+    .toString()
+    .padStart(2, "0");
+  return year + "年" + month + "月" + date + "日 ";
 }
 
 export function getYestaryToday(timestamp) {
   //判断是昨天的还是今天的消息  通过 日 来比较
-  let Time = new Date(timestamp)
+  let Time = new Date(timestamp);
   //当前日
-  const nowDate = (new Date().getDate()).toString().padStart(2, '0')
+  const nowDate = new Date()
+    .getDate()
+    .toString()
+    .padStart(2, "0");
   //数据日
-  const date = (Time.getDate()).toString().padStart(2, '0')
+  const date = Time.getDate()
+    .toString()
+    .padStart(2, "0");
 
-  let values
+  let values;
   if (nowDate == date) {
-    values = '今天'
+    values = "今天";
   } else {
-    values = "昨天"
+    values = "昨天";
   }
-  const hours = (Time.getHours()).toString().padStart(2, '0')
-  const minute = (Time.getMinutes()).toString().padStart(2, '0')
-  const second = (Time.getSeconds()).toString().padStart(2, '0')
-  return values + hours + ':' + minute + ':' + second
+  const hours = Time.getHours()
+    .toString()
+    .padStart(2, "0");
+  const minute = Time.getMinutes()
+    .toString()
+    .padStart(2, "0");
+  const second = Time.getSeconds()
+    .toString()
+    .padStart(2, "0");
+  return values + hours + ":" + minute + ":" + second;
 }
-
